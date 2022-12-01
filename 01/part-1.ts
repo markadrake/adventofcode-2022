53b5775c0a952604c27be0e8a8971836ed6b02e5
Mark Drake <markusdrake@gmail.com>
2022-12-01 00:08:19 -0700

    day 1

---
+++ 01/part-1.ts
@@ -0,0 +1,35 @@
+import * as fs from "fs";
+import readline from "readline";
+
+const elves : Array<number> = [];
+
+function processLineByLine(path: string) {
+
+	return new Promise<void>((resolve, reject) => {
+		let calories = 0;
+
+		var rd = readline.createInterface({
+			input: fs.createReadStream(path)
+		});
+		
+		rd.on("line", (line) => {
+	
+			if(!line) {
+				elves.push(calories);
+				calories = 0;
+				return;
+			}
+	
+			calories += parseInt(line);
+		});	
+	
+		rd.on("close", () => {
+			resolve();
+		});	
+	});
+}
+  
+processLineByLine("./input.txt").then(() => {
+	elves.sort((a, b) => a > b ? -1 : 1);
+	console.log(elves[0]);
+});
